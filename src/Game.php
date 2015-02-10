f48150fb91fa4ba0e58bbc4ca47fa96ed6e6d0ac
Alex Ross <alex@wearebase.com>
2015-02-10 14:07:22 +0000

    The third test
    Signed-off-by: Alex Ross <alex@wearebase.com>

--- src/Game.php
+++ src/Game.php
@@ -10,11 +10,22 @@ class Game
     private $score;
 
     /**
+     * @var int[]
+     */
+    private $rolls = [];
+
+    /**
+     * @var int
+     */
+    private $currentRoll = 0;
+
+    /**
      * @param int $pins
      */
     public function roll($pins)
     {
         $this->score += $pins;
+        $this->rolls[$this->currentRoll++] = $pins;
     }
 
     /**
@@ -22,6 +33,25 @@ class Game
      */
     public function score()
     {
-        return $this->score;
+        $score = 0;
+        $frameIndex = 0;
+        for ($frame = 0; $frame < 10; $frame++) {
+            if ($this->isSpare($frameIndex)) { // spare!
+                $score += 10 + $this->rolls[$frameIndex + 2];
+            } else {
+                $score += $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+            }
+            $frameIndex += 2;
+        }
+        return $score;
+    }
+
+    /**
+     * @param $frameIndex
+     * @return bool
+     */
+    private function isSpare($frameIndex)
+    {
+        return ($this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1]) === 10;
     }
 }
--- tests/GameTest.php
+++ tests/GameTest.php
@@ -42,4 +42,21 @@ class GameTest extends \PHPUnit_Framework_TestCase
         $this->rollMany(20, 1);
         $this->assertEquals(20, $this->bowlingGame->score());
     }
+
+    /**
+     * @throws \Exception
+     */
+    public function testOneSpare()
+    {
+        $this->rollSpare();
+        $this->bowlingGame->roll(3);
+        $this->rollMany(17, 0);
+        $this->assertEquals(16, $this->bowlingGame->score());
+    }
+
+    private function rollSpare()
+    {
+        $this->bowlingGame->roll(5);
+        $this->bowlingGame->roll(5);
+    }
 }
